c05e41436af875051b665ed0df5570f64efb0002
carlvskansas <48308402+carlvskansas@users.noreply.github.com>
2019-07-05 16:02:04 -0500

    mail.php

--- mail.php
+++ mail.php
@@ -2,33 +2,18 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$subject = $_POST['subject'];
-header('Content-Type: application/json');
-if ($name === ''){
-  print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-  exit();
-}
-if ($email === ''){
-  print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-  exit();
-} else {
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-  print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-  exit();
-  }
-}
-if ($subject === ''){
-  print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-  exit();
-}
-if ($message === ''){
-  print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-  exit();
-}
-$content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "carlgwsmith@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-exit();
+
+$email_from = "Contact Page Form";
+$email_subject = "New Submission from Contact Form";
+$email_body = "Name: $name./n".
+              "Email: $email./n".
+              "Message: $message./n";
+$to = "carlgwsmith@gmail.com";
+$headers = "From: $email_from /r/n";
+$headers .="Reply-To: $email /r/n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact.html");
+
 ?>
