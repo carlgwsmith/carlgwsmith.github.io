7342f809e76e226831cdcb4c109252741df76d75
Carl Smith <carlgwsmith@gmail.com>
2019-07-23 00:01:30 -0500

    push

--- thaidiner/js/index.js
+++ thaidiner/js/index.js
@@ -13,24 +13,5 @@ document.getElementById('starters').onclick = function fun(){
     alert('test');
     f1();
 }
-}};
-$(document).ready(function() {
-    $("nav").prepend('<div id="responsive-nav" style="display:none">Menu</div>');
-    $("#responsive-nav").on("click", function() {
-      $("nav ul").slideToggle();
-    });
-  
-    $(window).resize(function() {
-      if ($(window).innerWidth() < 768) {
-        $("nav ul li").css("display", "block");
-        $("nav ul").hide();
-        $("#responsive-nav").show();
-      } else {
-        $("nav ul li").css("display", "inline-block");
-        $("nav ul").show();
-        $("#responsive-nav").hide();
-      }
-    });
-  
-    $(window).resize();
-  });
+}}
+
